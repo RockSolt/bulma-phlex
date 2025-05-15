757d8e72559877cc7b1555adbf3ebe8e1e47b2bd
Todd Kummer <todd@rockridgesolutions.com>
2025-05-14 17:54:25 -0700

    bundle gem bulma_phlex

---
+++ lib/bulma_phlex/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BulmaPhlex
+  VERSION = "0.1.0"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "bulma_phlex"
+
+require "minitest/autorun"
