7b41605df388589ca0496e40ef8c0fb77106823a
Todd Kummer <todd@rockridgesolutions.com>
2025-06-28 14:24:02 -0700

    Tabs Enhancements
    - allow the tabs class to be passed in (previously defaulted to `is-boxed`, so this is a change)
    - wrap the contents in a div and allow a class to be passed in
    - add content to the right of the tabs, such as a button or display

--- lib/components/bulma/tabs.rb
+++ lib/components/bulma/tabs.rb
@@ -8,6 +8,11 @@ module Components
     # interface, providing a way to toggle between different content sections using
     # tabbed navigation. Includes support for icons and active state management.
     #
+    # Classes can be assigned to either the tabs or contents wrappers. The tabs div is where Bulma
+    # options such as `is-boxed`, `is-centered`, or `is-small` can be added.
+    #
+    # Use method `right_content` to add content to the right of the tabs, such as a button.
+    #
     # ## Example
     #
     # ```ruby
@@ -30,7 +35,9 @@ module Components
       Tab = Data.define(:id, :title, :icon, :active)
       Content = Data.define(:id, :block, :active)
 
-      def initialize(stimulus_controller: "bulma--tabs")
+      def initialize(tabs_class: nil, contents_class: nil, stimulus_controller: "bulma--tabs")
+        @tabs_class = tabs_class
+        @contents_class = contents_class
         @stimulus_controller = stimulus_controller
         @tabs = []
         @contents = []
@@ -41,42 +48,62 @@ module Components
         @contents << Content.new(id:, block:, active:)
       end
 
+      def right_content(&content)
+        @right_content = content
+      end
+
       def view_template(&)
         vanish(&)
 
         div(data: { controller: @stimulus_controller }) do
-          div(class: "tabs is-boxed") do
-            ul do
-              @tabs.each do |tab|
-                li(
-                  id: "#{tab.id}-tab",
-                  data: {
-                    target_key => "tab",
-                    tab_content: tab.id,
-                    action: "click->#{@stimulus_controller}#showTabContent"
-                  },
-                  class: tab.active ? "is-active" : ""
-                ) do
-                  a do
-                    icon_span(tab.icon, "mr-1") if tab.icon
-                    span { tab.title }
-                  end
+          build_tabs_with_optional_right_content
+          div(class: @contents_class) { build_content }
+        end
+      end
+
+      private
+
+      def build_tabs_with_optional_right_content
+        return build_tabs if @right_content.nil?
+
+        div(class: "columns") do
+          div(class: "column") { build_tabs }
+          div(class: "column is-narrow") { @right_content.call }
+        end
+      end
+
+      def build_tabs
+        div(class: "tabs #{@tabs_class}".strip) do
+          ul do
+            @tabs.each do |tab|
+              li(
+                id: "#{tab.id}-tab",
+                data: {
+                  target_key => "tab",
+                  tab_content: tab.id,
+                  action: "click->#{@stimulus_controller}#showTabContent"
+                },
+                class: tab.active ? "is-active" : ""
+              ) do
+                a do
+                  icon_span(tab.icon, "mr-1") if tab.icon
+                  span { tab.title }
                 end
               end
-            end
-          end
-
-          @contents.each do |content|
-            div(id: content.id,
-                class: content.active ? "" : "is-hidden",
-                data: { target_key => "content" }) do
-              content.block.call
             end
           end
         end
       end
 
-      private
+      def build_content
+        @contents.each do |content|
+          div(id: content.id,
+              class: content.active ? "" : "is-hidden",
+              data: { target_key => "content" }) do
+            content.block.call
+          end
+        end
+      end
 
       def target_key
         "#{@stimulus_controller}-target"
--- test/components/bulma/tabs_test.rb
+++ test/components/bulma/tabs_test.rb
@@ -11,18 +11,12 @@ module Components
         component = Components::Bulma::Tabs.new
 
         result = component.call do |tabs|
-          tabs.tab(id: "tab1", title: "Tab 1", active: true) do
-            "Content for Tab 1"
-          end
-
-          tabs.tab(id: "tab2", title: "Tab 2") do
-            "Content for Tab 2"
-          end
+          assign_two_tabs(tabs)
         end
 
         expected_structure = <<~HTML
           <div data-controller="bulma--tabs">
-            <div class="tabs is-boxed">
+            <div class="tabs">
               <ul>
                 <li id="tab1-tab" data-bulma--tabs-target="tab" data-tab-content="tab1" data-action="click->bulma--tabs#showTabContent" class="is-active">
                   <a><span>Tab 1</span></a>
@@ -32,12 +26,75 @@ module Components
                 </li>
               </ul>
             </div>
-            <div id="tab1" class="" data-bulma--tabs-target="content">Content for Tab 1</div>
-            <div id="tab2" class="is-hidden" data-bulma--tabs-target="content">Content for Tab 2</div>
+
+            <div>
+              <div id="tab1" class="" data-bulma--tabs-target="content">Content for Tab 1</div>
+              <div id="tab2" class="is-hidden" data-bulma--tabs-target="content">Content for Tab 2</div>
+            </div>
           </div>
         HTML
 
         assert_html_equal expected_structure, result
+      end
+
+      def test_renders_tabs_with_flex_right
+        component = Components::Bulma::Tabs.new
+
+        result = component.call do |tabs|
+          assign_two_tabs(tabs)
+          tabs.right_content { "ON THE RIGHT" }
+        end
+
+        expected_structure = <<~HTML
+          <div data-controller="bulma--tabs">
+            <div class="columns">
+              <div class="column">
+                <div class="tabs">
+                <ul>
+                    <li id="tab1-tab" data-bulma--tabs-target="tab" data-tab-content="tab1" data-action="click->bulma--tabs#showTabContent" class="is-active">
+                    <a><span>Tab 1</span></a>
+                    </li>
+                    <li id="tab2-tab" data-bulma--tabs-target="tab" data-tab-content="tab2" data-action="click->bulma--tabs#showTabContent" class="">
+                    <a><span>Tab 2</span></a>
+                    </li>
+                </ul>
+                </div>
+              </div>
+              <div class="column is-narrow">ON THE RIGHT</div>
+            </div>
+
+            <div>
+              <div id="tab1" class="" data-bulma--tabs-target="content">Content for Tab 1</div>
+              <div id="tab2" class="is-hidden" data-bulma--tabs-target="content">Content for Tab 2</div>
+            </div>
+          </div>
+        HTML
+
+        assert_html_equal expected_structure, result
+      end
+
+      def test_tabs_class
+        component = Components::Bulma::Tabs.new(tabs_class: "is-boxed")
+
+        result = component.call do |tabs|
+          tabs.tab(id: "tab1", title: "Settings", icon: "fas fa-cog") do
+            "Settings content"
+          end
+        end
+
+        assert_html_includes result, '<div class="tabs is-boxed">'
+      end
+
+      def test_contents_class
+        component = Components::Bulma::Tabs.new(contents_class: "ml-4")
+
+        result = component.call do |tabs|
+          tabs.tab(id: "tab1", title: "Settings", icon: "fas fa-cog") do
+            "Settings content"
+          end
+        end
+
+        assert_html_includes result, '<div class="ml-4">'
       end
 
       def test_renders_tabs_with_icons
@@ -51,6 +108,18 @@ module Components
 
         assert_html_includes result, '<span class="icon mr-1"><i class="fas fa-cog"></i></span>'
         assert_html_includes result, "<span>Settings</span>"
+      end
+
+      private
+
+      def assign_two_tabs(tabs)
+        tabs.tab(id: "tab1", title: "Tab 1", active: true) do
+          "Content for Tab 1"
+        end
+
+        tabs.tab(id: "tab2", title: "Tab 2") do
+          "Content for Tab 2"
+        end
       end
     end
   end
