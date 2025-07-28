0cb14675a6641b2a8106204a0f3d4a73fa757c38
Todd Kummer <toddkummer@users.noreply.github.com>
2025-07-28 16:11:06 -0700

    Switch to Zeitwerk (#16)
    * Add zeitwerk to gemspec
    
    * Switch to Zeitwork
    
    This does not use the `for_gem` method because the components are not namespaced under Bulma::Phlex.

--- lib/bulma-phlex.rb
+++ lib/bulma-phlex.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
-require "phlex"
 require "bulma_phlex/version"
+require "zeitwerk"
+require "phlex"
 
-require "components/bulma"
-require "components/bulma/base"
-require "components/bulma/card"
-require "components/bulma/dropdown"
-require "components/bulma/level"
-require "components/bulma/navigation_bar_dropdown"
-require "components/bulma/navigation_bar"
-require "components/bulma/pagination"
-require "components/bulma/table"
-require "components/bulma/tabs"
-require "components/bulma/tab_components/content"
-require "components/bulma/tab_components/tab"
+loader = Zeitwerk::Loader.new
+loader.tag = "bulma-phlex"
+loader.push_dir(File.dirname(__FILE__))
+loader.ignore(__FILE__)
+loader.setup
