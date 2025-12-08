724695ff15e1e32f2e38f2fd0818d0cfcc6bad50
Todd Kummer <todd@rockridgesolutions.com>
2025-12-08 14:16:25 -0800

    Change namespace: Components::Bulma -> BulmaPhlex

--- lib/bulma_phlex/base.rb
+++ lib/bulma_phlex/base.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-module Components
-  module Bulma
-    # Base component for all Bulma components
-    #
-    # This is the parent class for all Bulma components in this library.
-    # It provides common utility methods and inherits from `Phlex::HTML`.
-    #
-    class Base < Phlex::HTML
-      private
+module BulmaPhlex
+  # Base component for all Bulma components
+  #
+  # This is the parent class for all Bulma components in this library.
+  # It provides common utility methods and inherits from `Phlex::HTML`.
+  #
+  class Base < Phlex::HTML
+    private
 
-      def icon_span(icon, additional_classes = nil)
-        span(class: "icon #{additional_classes}".strip) do
-          i(class: icon)
-        end
+    def icon_span(icon, additional_classes = nil)
+      span(class: "icon #{additional_classes}".strip) do
+        i(class: icon)
       end
     end
   end
--- lib/bulma_phlex/level.rb
+++ lib/bulma_phlex/level.rb
@@ -1,69 +1,67 @@
 # frozen_string_literal: true
 
-module Components
-  module Bulma
-    # Level component for responsive horizontal layouts
-    #
-    # This component implements the [Bulma level](https://bulma.io/documentation/layout/level/)
-    # interface, providing a flexible horizontal layout system with left and right alignment.
-    #
-    # ## Example:
-    #
-    # ```ruby
-    # Bulma::Level() do |level|
-    #   level.left do
-    #     button(class: "button") { "Left" }
-    #   end
-    #
-    #   level.right do
-    #     button(class: "button") { "Right" }
-    #   end
-    #   level.right do
-    #     button(class: "button") { "Right 2" }
-    #   end
-    # end
-    # ```
-    #
-    class Level < Components::Bulma::Base
-      def initialize
-        @items = []
-        @left = []
-        @right = []
-      end
+module BulmaPhlex
+  # Level component for responsive horizontal layouts
+  #
+  # This component implements the [Bulma level](https://bulma.io/documentation/layout/level/)
+  # interface, providing a flexible horizontal layout system with left and right alignment.
+  #
+  # ## Example:
+  #
+  # ```ruby
+  # Bulma::Level() do |level|
+  #   level.left do
+  #     button(class: "button") { "Left" }
+  #   end
+  #
+  #   level.right do
+  #     button(class: "button") { "Right" }
+  #   end
+  #   level.right do
+  #     button(class: "button") { "Right 2" }
+  #   end
+  # end
+  # ```
+  #
+  class Level < Components::Bulma::Base
+    def initialize
+      @items = []
+      @left = []
+      @right = []
+    end
 
-      def view_template(&)
-        vanish(&)
+    def view_template(&)
+      vanish(&)
 
-        div(class: "level") do
-          div(class: "level-left") do
-            @left.each { level_item(it) }
-          end
+      div(class: "level") do
+        div(class: "level-left") do
+          @left.each { level_item(it) }
+        end
 
-          @items.each { level_item(it) }
+        @items.each { level_item(it) }
 
-          div(class: "level-right") do
-            @right.each { level_item(it) }
-          end
+        div(class: "level-right") do
+          @right.each { level_item(it) }
         end
       end
+    end
 
-      def item(&content)
-        @items << content
-      end
+    def item(&content)
+      @items << content
+    end
 
-      def left(&content)
-        @left << content
-      end
+    def left(&content)
+      @left << content
+    end
 
-      def right(&content)
-        @right << content
-      end
+    def right(&content)
+      @right << content
+    end
 
-      private
+    private
 
-      def level_item(content)
-        div(class: "level-item") { content.call }
-      end
+    def level_item(content)
+      div(class: "level-item") { content.call }
     end
   end
 end
