f3f2d5799f82aded63b315c79ac204c081f8b14b
Todd Kummer <todd@rockridgesolutions.com>
2025-12-08 14:16:25 -0800

    Change namespace: Components::Bulma -> BulmaPhlex

--- lib/bulma_phlex/card.rb
+++ lib/bulma_phlex/card.rb
@@ -1,81 +1,79 @@
 # frozen_string_literal: true
 
-module Components
-  module Bulma
-    # Card component for content display
-    #
-    # This component implements the [Bulma card](https://bulma.io/documentation/components/card/)
-    # interface. Cards are flexible containers that can display various types of content
-    # including headers, content sections, and more.
-    #
-    # ## Example
-    #
-    # ```ruby
-    # Bulma::Card() do |card|
-    #   card.head("Card Title")
-    #   card.content do
-    #     "This is some card content"
-    #   end
-    #   card.footer_link("View", "/view", target: "_blank")
-    #   card.footer_link("Edit", "/edit", class: "has-text-primary")
-    # end
-    # ```
-    #
-    # ## Rails Feature: Turbo Frame Content
-    #
-    # If the project includes Rails and the Phlex::Rails gem, the `BulmaPhlex::Rails::CardHelper` module
-    # provides a `turbo_frame_content` method to create a card with a turbo frame
-    # as its content. This allows for dynamic loading of card content.
-    class Card < Components::Bulma::Base
-      def view_template(&)
-        vanish(&)
+module BulmaPhlex
+  # Card component for content display
+  #
+  # This component implements the [Bulma card](https://bulma.io/documentation/components/card/)
+  # interface. Cards are flexible containers that can display various types of content
+  # including headers, content sections, and more.
+  #
+  # ## Example
+  #
+  # ```ruby
+  # Bulma::Card() do |card|
+  #   card.head("Card Title")
+  #   card.content do
+  #     "This is some card content"
+  #   end
+  #   card.footer_link("View", "/view", target: "_blank")
+  #   card.footer_link("Edit", "/edit", class: "has-text-primary")
+  # end
+  # ```
+  #
+  # ## Rails Feature: Turbo Frame Content
+  #
+  # If the project includes Rails and the Phlex::Rails gem, the `BulmaPhlex::Rails::CardHelper` module
+  # provides a `turbo_frame_content` method to create a card with a turbo frame
+  # as its content. This allows for dynamic loading of card content.
+  class Card < Components::Bulma::Base
+    def view_template(&)
+      vanish(&)
 
-        div(class: "card") do
-          card_header
-          card_content
-          card_footer
-        end
+      div(class: "card") do
+        card_header
+        card_content
+        card_footer
       end
+    end
 
-      def head(title, classes: nil)
-        @header_title = title
-        @header_classes = classes
+    def head(title, classes: nil)
+      @header_title = title
+      @header_classes = classes
+    end
+
+    def content(&block)
+      @card_content = block
+    end
+
+    def footer_link(text, href, **html_attributes)
+      (@footer_items ||= []) << [text, href, html_attributes]
+    end
+
+    private
+
+    def card_header
+      return if @header_title.nil?
+
+      header(class: "card-header #{@header_classes}") do
+        p(class: "card-header-title") { plain @header_title }
       end
+    end
 
-      def content(&block)
-        @card_content = block
+    def card_content
+      return if @card_content.nil?
+
+      div(class: "card-content") do
+        div(class: "content") { @card_content.call }
       end
+    end
 
-      def footer_link(text, href, **html_attributes)
-        (@footer_items ||= []) << [text, href, html_attributes]
-      end
+    def card_footer
+      return if @footer_items.nil? || @footer_items.empty?
 
-      private
-
-      def card_header
-        return if @header_title.nil?
-
-        header(class: "card-header #{@header_classes}") do
-          p(class: "card-header-title") { plain @header_title }
-        end
-      end
-
-      def card_content
-        return if @card_content.nil?
-
-        div(class: "card-content") do
-          div(class: "content") { @card_content.call }
-        end
-      end
-
-      def card_footer
-        return if @footer_items.nil? || @footer_items.empty?
-
-        footer(class: "card-footer") do
-          @footer_items.each do |text, href, html_attributes|
-            html_attributes[:class] = [html_attributes[:class], "card-footer-item"].compact.join(" ")
-            a(href:, **html_attributes) { text }
-          end
+      footer(class: "card-footer") do
+        @footer_items.each do |text, href, html_attributes|
+          html_attributes[:class] = [html_attributes[:class], "card-footer-item"].compact.join(" ")
+          a(href:, **html_attributes) { text }
         end
       end
     end
--- lib/bulma_phlex/navigation_bar.rb
+++ lib/bulma_phlex/navigation_bar.rb
@@ -1,107 +1,105 @@
 # frozen_string_literal: true
 
-module Components
-  module Bulma
-    # Navigation bar component for site navigation
-    #
-    # This component implements the [Bulma navbar](https://bulma.io/documentation/components/navbar/)
-    # interface. It provides a responsive navigation header with support for branding, navigation
-    # links, and dropdown menus, automatically collapsing on mobile devices.
-    #
-    # ## Example
-    #
-    # ```ruby
-    # Bulma::NavigationBar() do |navbar|
-    #   navbar.brand do
-    #     a(href: "/", class: "navbar-item") { "My App" }
-    #   end
-    #
-    #   navbar.left do
-    #     a(href: "/", class: "navbar-item") { "Home" }
-    #     a(href: "/products", class: "navbar-item") { "Products" }
-    #   end
-    #
-    #   navbar.right do
-    #     a(href: "/about", class: "navbar-item") { "About" }
-    #
-    #     div(class: "navbar-item has-dropdown is-hoverable") do
-    #       a(class: "navbar-link") { "Account" }
-    #       Bulma::NavigationBarDropdown() do |dropdown|
-    #         dropdown.item "Sign In", "/login"
-    #         dropdown.item "Register", "/register"
-    #       end
-    #     end
-    #   end
-    # end
-    # ```
-    #
-    class NavigationBar < Components::Bulma::Base
-      def initialize(container: false, classes: "")
-        @container = container
-        @classes = classes
-        @brand = []
-        @left = []
-        @right = []
-      end
+module BulmaPhlex
+  # Navigation bar component for site navigation
+  #
+  # This component implements the [Bulma navbar](https://bulma.io/documentation/components/navbar/)
+  # interface. It provides a responsive navigation header with support for branding, navigation
+  # links, and dropdown menus, automatically collapsing on mobile devices.
+  #
+  # ## Example
+  #
+  # ```ruby
+  # Bulma::NavigationBar() do |navbar|
+  #   navbar.brand do
+  #     a(href: "/", class: "navbar-item") { "My App" }
+  #   end
+  #
+  #   navbar.left do
+  #     a(href: "/", class: "navbar-item") { "Home" }
+  #     a(href: "/products", class: "navbar-item") { "Products" }
+  #   end
+  #
+  #   navbar.right do
+  #     a(href: "/about", class: "navbar-item") { "About" }
+  #
+  #     div(class: "navbar-item has-dropdown is-hoverable") do
+  #       a(class: "navbar-link") { "Account" }
+  #       Bulma::NavigationBarDropdown() do |dropdown|
+  #         dropdown.item "Sign In", "/login"
+  #         dropdown.item "Register", "/register"
+  #       end
+  #     end
+  #   end
+  # end
+  # ```
+  #
+  class NavigationBar < Components::Bulma::Base
+    def initialize(container: false, classes: "")
+      @container = container
+      @classes = classes
+      @brand = []
+      @left = []
+      @right = []
+    end
 
-      def view_template(&)
-        vanish(&)
+    def view_template(&)
+      vanish(&)
 
-        nav(class: "navbar #{@classes}".rstrip,
-            role: "navigation",
-            aria_label: "main navigation",
-            data: { controller: "bulma--navigation-bar" }) do
-          optional_container do
-            div(class: "navbar-brand") do
-              @brand.each(&:call)
+      nav(class: "navbar #{@classes}".rstrip,
+          role: "navigation",
+          aria_label: "main navigation",
+          data: { controller: "bulma--navigation-bar" }) do
+        optional_container do
+          div(class: "navbar-brand") do
+            @brand.each(&:call)
 
-              a(class: "navbar-burger",
-                role: "button",
-                aria_label: "menu",
-                aria_expanded: "false",
-                data: {
-                  action: "bulma--navigation-bar#toggle",
-                  "bulma--navigation-bar-target": "burger"
-                }) do
-                4.times { span(aria_hidden: "true") }
-              end
+            a(class: "navbar-burger",
+              role: "button",
+              aria_label: "menu",
+              aria_expanded: "false",
+              data: {
+                action: "bulma--navigation-bar#toggle",
+                "bulma--navigation-bar-target": "burger"
+              }) do
+              4.times { span(aria_hidden: "true") }
+            end
+          end
+
+          div(class: "navbar-menu",
+              data: { "bulma--navigation-bar-target": "menu" }) do
+            div(class: "navbar-start") do
+              @left.each(&:call)
             end
 
-            div(class: "navbar-menu",
-                data: { "bulma--navigation-bar-target": "menu" }) do
-              div(class: "navbar-start") do
-                @left.each(&:call)
-              end
-
-              div(class: "navbar-end") do
-                @right.each(&:call)
-              end
+            div(class: "navbar-end") do
+              @right.each(&:call)
             end
           end
         end
       end
+    end
 
-      def brand(&block)
-        @brand << block
-      end
+    def brand(&block)
+      @brand << block
+    end
 
-      def left(&block)
-        @left << block
-      end
+    def left(&block)
+      @left << block
+    end
 
-      def right(&block)
-        @right << block
-      end
+    def right(&block)
+      @right << block
+    end
 
-      private
+    private
 
-      def optional_container(&)
-        if @container
-          constraint = @container if @container.is_a?(String) || @container.is_a?(Symbol)
-          div(class: "container #{constraint}".rstrip, &)
-        else
-          yield
-        end
+    def optional_container(&)
+      if @container
+        constraint = @container if @container.is_a?(String) || @container.is_a?(Symbol)
+        div(class: "container #{constraint}".rstrip, &)
+      else
+        yield
       end
     end
   end
