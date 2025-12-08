26a85d88a584a10b9fb192eff3ed59ddf323d45f
Todd Kummer <todd@rockridgesolutions.com>
2025-12-08 14:02:06 -0800

    Switch to Zeitwerk loaded BulmaPhlex namespace
    This introduces `lib/bulma_phlex.rb` (note the underscore), and uses it to invoke zeitwerk.

--- lib/bulma-phlex.rb
+++ lib/bulma-phlex.rb
@@ -1,17 +1,3 @@
 # frozen_string_literal: true
 
-require "bulma_phlex/version"
-require "phlex"
-
-require "components/bulma"
-require "components/bulma/base"
-require "components/bulma/card"
-require "components/bulma/dropdown"
-require "components/bulma/level"
-require "components/bulma/navigation_bar_dropdown"
-require "components/bulma/navigation_bar"
-require "components/bulma/pagination"
-require "components/bulma/table"
-require "components/bulma/tabs"
-require "components/bulma/tab_components/content"
-require "components/bulma/tab_components/tab"
+require "bulma_phlex"
---
+++ lib/bulma_phlex.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "phlex"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.setup # ready!
+
+# A collection of Bulma components built with Phlex
+#
+# This module provides a set of components that implement the [Bulma CSS framework](https://bulma.io/)
+# using the Phlex view component library. These components make it easy to build
+# Bulma-styled applications with a Ruby-focused components.
+module BulmaPhlex
+  extend Phlex::Kit
+end
