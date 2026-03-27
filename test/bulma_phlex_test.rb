ae1bbcd6e61c71c337423a93bec92c1cae9cc3ca
Todd Kummer <todd@rockridgesolutions.com>
2026-03-27 11:33:16 -0700

    Rename BulmaPhlexTest
    This matches the project conventions.

--- test/bulma_phlex_test.rb
+++ test/bulma_phlex_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class TestBulmaPhlex < Minitest::Test
+class BulmaPhlexTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::BulmaPhlex::VERSION
   end
