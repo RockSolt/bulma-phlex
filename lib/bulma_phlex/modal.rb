133d8fadea82db35c6994b6754d47031af407eb1
Todd Kummer <todd@rockridgesolutions.com>
2026-08-06 17:04:47 -0700

    Container actions for escape key and Invoker API
    This adds two actions to the default Stimulus data attributes:
    - an escape key listener to close the modal
    - a command listener to open or close the modal

--- lib/bulma_phlex/modal.rb
+++ lib/bulma_phlex/modal.rb
@@ -8,7 +8,12 @@ module BulmaPhlex
   # `data_attributes_builder` option.
   class Modal < BulmaPhlex::Base
     StimulusDataAttributes = Data.define(:stimulus_controller) do
-      def for_container = { controller: stimulus_controller }
+      def for_container
+        { controller: stimulus_controller,
+          action: ["keydown.esc@document->#{stimulus_controller}#close",
+                   "command->#{stimulus_controller}#openOrCloseByCommand"] }
+      end
+
       def for_background = { action: "click->#{stimulus_controller}#close" }
       def for_close_button = { action: "#{stimulus_controller}#close" }
     end
